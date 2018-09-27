dee157c2b1e9efec6c306f26989fbe3c9573828e
Kristian Estrada <KristianEstrada209@gmail.com>
2018-09-27 11:40:01 -0700

    Updated and mysql password

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', '7UG80tYUOd2VfovO', 'phpblog');
+$conn = mysqli_connect('localhost', 'root', '1234', 'phpblog');
 
 if(mysqli_connect_errno()){
   echo 'Failed to connect to MySQL database '.mysqli_connect_errno();
