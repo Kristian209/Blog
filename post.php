152c2604dbaa9571cbb619cb56f6e58202ddd09d
Kristian Estrada <KristianEstrada209@gmail.com>
2018-09-29 17:45:14 -0700

    Added a delete option

--- post.php
+++ post.php
@@ -2,6 +2,18 @@
 require('config/config.php');
 require('config/db.php');
 
+if(isset($_POST['delete'])){
+  $delete_id = mysqli_real_escape_string($conn, $_POST['delete_id']);
+
+  $query = "DELETE FROM posts WHERE Id = {$delete_id}";
+
+  if(mysqli_query($conn, $query)){
+    header('Location: '.ROOT_URL.'');
+  }else{
+    echo "Error".mysqli_error($conn);
+  }
+};
+
 $id = mysqli_real_escape_string($conn, $_GET['id']);
 
 //Create query
@@ -26,8 +38,12 @@ mysqli_close($conn);
 <h1><?php echo $post['Title']; ?></h1>
 <small><?php echo $post['Created_at']; ?> by <?php echo $post['Author']; ?></small>
 <p><?php echo $post['Body']; ?></p>
-<a class="btn-default" href="<?php echo ROOT_URL ?>">Back</a>
+<a class="btn-alert" href="<?php echo ROOT_URL ?>">Back</a>
   </body>
   <hr>
-  <a class="btn-default" href="<?php echo ROOT_URL; ?>editpost.php?id=<?php echo $post['Id']; ?>">Edit</a>
+  <a class="btn-alert" href="<?php echo ROOT_URL; ?>editpost.php?id=<?php echo $post['Id']; ?>">Edit</a>
+  <form class="pull-right" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+    <input type="hidden" name="delete_id" value="<?php echo $post['Id'] ?>">
+    <input type="submit" name="delete" value="Delete" class="btn-dangers">
+  </form>
 </html>
